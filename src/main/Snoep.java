e6d5acba599699c1b9454a4aea53aba2980c1e0f
Bryanrobu <1211898@student.roc-nijmegen.nl>
2025-02-27 12:53:31 +0100

    Linux supremacy

---
+++ src/main/Snoep.java
@@ -0,0 +1,23 @@
+package main;
+
+public class Snoep
+{
+    private String naam;
+    private String omschrijving;
+
+    public Snoep(String placeholderNaam, String placeholderOmschrijving)
+    {
+        naam = placeholderNaam;
+        omschrijving = placeholderOmschrijving;
+    }
+
+    public String KrijgNaam()
+    {
+        return naam;
+    }
+
+    public String KrijgOmschrijving()
+    {
+        return omschrijving;
+    }
+}
---
+++ src/main/SnoepAutomaat.java
@@ -0,0 +1,25 @@
+package main;
+
+import jdk.jshell.SnippetEvent;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class SnoepAutomaat
+{
+
+    ArrayList<Snoep> snoepjes = new ArrayList<Snoep>();
+    Snoep mars = new Snoep("Mars", "Een heerlijke chocolade delicatesse met een mix van koek en karamel erin.");
+
+
+    private void KnopIndrukkenVoorSnoep()
+    {
+
+    }
+
+    private void KnopIndrukkenVoorInformatie()
+    {
+
+    }
+}
+
